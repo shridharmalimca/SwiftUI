b542e6ec835e333343799c1c6392503d85b4796b
Shridhar Mali <shridharmalimca@gmail.com>
2022-11-28 16:52:28 +0530

    git ignore modified

---
+++ StateObjectDemo/StateObjectDemo/StateObjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectDemoApp.swift
+//  StateObjectDemo
+//
+//  Created by Macbook on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
